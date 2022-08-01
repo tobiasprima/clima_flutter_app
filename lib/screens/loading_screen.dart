236c76b784bf3f5b562a686e0b20971cef5e7cb1
tobiasprima <88434271+tobiasprima@users.noreply.github.com>
2022-08-01 22:48:10 +0700

    Import Geolocator and http

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import 'package:clima_flutter_app/services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,14 +7,21 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
   }
 
-  @override
+  void getLocation() async {
+    Location location = Location();
+    await location.getCurrentLocation();
+    print(location.latitude);
+    print(location.longitude);
+  }
+
   Widget build(BuildContext context) {
+    getLocation();
     return Scaffold(
       body: Center(
         child: ElevatedButton(
